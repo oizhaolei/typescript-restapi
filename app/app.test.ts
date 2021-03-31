2e4ce10bad81cf7b6a2072650cae1ea53b433b25
lei <oizhaolei@gmail.com>
2021-03-31 22:53:48 +0900

    jest

--- app/app.test.ts
+++ app/app.test.ts
@@ -1,18 +1,30 @@
-import request from 'supertest';
+import supertest from 'supertest';
 import App from './app';
 
-describe('Test the root path', async () => {
-  const { app, server } = await App();
-
+describe('Test the root path', () => {
   test('GET: /', async () => {
-    const response = await request(app).get('/');
+    const { app } = await App();
+
+    const request = supertest(app);
+    const response = await request.get('/');
     expect(response.status).toBe(200);
     expect(response.text).toEqual('Hello World!');
   });
 
-  test(`GET: ${server.graphqlPath}`, async () => {
-    const response = await request(app).get(server.graphqlPath);
+  test('GET: graphqlPath', async () => {
+    const { app, server } = await App();
+
+    const request = supertest(app);
+    const response = await request.post(server.graphqlPath).send({
+      query: `query returnAllCategories {
+  returnAllCategories {
+    id
+    name
+    description
+  }
+}`,
+    });
     expect(response.status).toBe(200);
-    expect(response.text).toEqual('text');
+    expect(response.body.data.returnAllCategories.length).toBe(0);
   });
 });
